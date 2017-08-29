2397b7193463e4fc50a60d62fbec120fcbb0b582
ilker ALTIN <ilkerobi@gmail.com>
2017-08-29 10:06:56 +0300

    heroku edits

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -59,6 +60,6 @@ app.get('/about', (req,res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('server is up at port 3000')
+app.listen(port, () => {
+    console.log(`server is up at port ${port}`)
 });
